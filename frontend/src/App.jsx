12cd0612f9836d8398f1b31118796258b81a4b71
VANSH <vanshhingrajiya7@gmail.com>
2025-12-22 13:42:49 +0530

    Added snowfall theme.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,29 @@ import SkillsSection from '../sections/SkillsSection'
 import ProjectSection from '../sections/ProjectSection'
 import ContactSection from '../sections/ContactSection'
 import Footer from '../sections/Footer'
+import Snowfall from "react-snowfall";
 import "../style/App.css"
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <HeroSection />
-      <AboutSection />
-      <SkillsSection />
-      <ProjectSection />
-      <ContactSection />
-      <Footer />
+      <div className="app-root">
+        <Snowfall
+          snowflakeCount={500}
+          color="white"
+          radius={[0.5, 2.5]}
+          speed={[0.5, 2]}
+          wind={[-0.5, 2]}
+        />
+      </div>
+
+        <Navbar />
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectSection />
+        <ContactSection />
+        <Footer />
     </>
   )
 }
